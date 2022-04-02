1e8dcb24fb1cf458e56e30c0f7d02d5af4949d60
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-02 16:49:16 +0530

    made the template for theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`
+  }
+}
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tclr: withOpacityValue('--txt-color'),
+        bgclr: withOpacityValue('--bkg-color'),
+        // ...
+      }
+    },
   },
   plugins: [],
 }
