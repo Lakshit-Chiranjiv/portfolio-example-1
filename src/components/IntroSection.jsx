4613f4b80e81daa66087625f9da06e85857f9ee1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-09 00:18:38 +0530

    intro section completer

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -14,9 +14,9 @@ const IntroSection = () => {
       <div className='bg-bgclr w-11/12 m-10 mx-auto rounded-md p-8 sm:bg-red-500 md:bg-blue-500 lg:bg-green-400 xl:bg-yellow-300 flex flex-col gap-4 justify-center items-center'>
           <EmoCard/>
           <div className='flex gap-4'>
-            <SocialLink socialIcon={<FaGithub size={28}/>} socialLinkName='Github' show={showGithub} setShow={setShowGithub} soloStyles="bg-white"/>
-            <SocialLink socialIcon={<FaLinkedin size={28}/>} socialLinkName='LinkedIn' show={showLinkedin} setShow={setShowLinkedin} soloStyles="bg-blue-600 text-white"/>
-            <SocialLink socialIcon={<FaTwitter size={28}/>} socialLinkName='Twitter' show={showTwitter} setShow={setShowTwitter} soloStyles="bg-sky-500 text-white"/>
+            <SocialLink socialIcon={<FaGithub size={28}/>} socialLinkName='Github' show={showGithub} setShow={setShowGithub} soloStyles="bg-white" linkTo="https://github.com/Lakshit-Chiranjiv"/>
+            <SocialLink socialIcon={<FaLinkedin size={28}/>} socialLinkName='LinkedIn' show={showLinkedin} setShow={setShowLinkedin} soloStyles="bg-blue-600 text-white" linkTo="https://www.linkedin.com/in/lakshit-chiranjiv-sagar-4b0b15198/"/>
+            <SocialLink socialIcon={<FaTwitter size={28}/>} socialLinkName='Twitter' show={showTwitter} setShow={setShowTwitter} soloStyles="bg-sky-500 text-white" linkTo="https://twitter.com/lakshit_cs"/>
           </div>
       </div>
 
--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const SocialLink = ({socialIcon,socialLinkName,show,setShow,soloStyles}) => {
+const SocialLink = ({socialIcon,socialLinkName,show,setShow,soloStyles,linkTo}) => {
   return (
-    <a href="#">
+    <a href={linkTo} target='_blank'>
       <div className={`w-16 p-4 m-3 rounded-md cursor-pointer transition-width hover:w-40 duration-300 flex gap-4 ${soloStyles} shadow-lg`} onMouseEnter={()=>{setShow(true)}} onMouseLeave={()=>{setShow(false)}}>
           <p className="">{socialIcon}</p>
           <p className={`text-lg transition-display duration-600 ${(!show && `hidden`)} font-bold`}>{socialLinkName}</p>
