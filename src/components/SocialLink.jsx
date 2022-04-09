13b461ce54ea8fe76f4f49c93460f568e1ad493e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-10 01:09:58 +0530

    minor correction in social links

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const SocialLink = ({socialIcon,socialLinkName,show,setShow,soloStyles,linkTo}) => {
   return (
     <a href={linkTo} target='_blank'>
-      <div className={`w-16 p-4 m-3 rounded-md cursor-pointer transition-width hover:w-40 duration-300 flex gap-4 ${soloStyles} shadow-lg`} onMouseEnter={()=>{setShow(true)}} onMouseLeave={()=>{setShow(false)}}>
+      <div className={`w-16 p-4 m-3 rounded-md cursor-pointer transition-width hover:w-40 duration-300 flex gap-4 ${soloStyles} shadow-lg`} 
+      onMouseEnter={()=>{
+        setTimeout(()=>setShow(true),50);
+      }} 
+      onMouseLeave={()=>{
+        setTimeout(()=>setShow(false),50);
+      }}>
           <p className="">{socialIcon}</p>
           <p className={`text-lg ${(!show && `hidden`)} font-bold`}>{socialLinkName}</p>
       </div>
