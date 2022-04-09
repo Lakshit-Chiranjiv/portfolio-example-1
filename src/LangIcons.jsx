9a033084342a359acf2dfcdc15ffb6dc52b5e17e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-10 00:51:27 +0530

    created a separate file for listing langs data

---
+++ src/LangIcons.jsx
@@ -0,0 +1,165 @@
+import {SiJavascript,SiHtml5,SiCss3,SiBootstrap,SiTailwindcss,SiReact,SiRedux,SiC,SiCplusplus,SiJava,SiPython,SiReactrouter,SiMaterialui} from 'react-icons/si';
+
+const langiconsObj = {
+    html: {
+        name: 'Html5',
+        icon: <SiHtml5/>,
+        color: 'text-orange-400',
+        page: 1
+    } ,
+    css: {
+        name: 'Css3',
+        icon: <SiCss3/>,
+        color: 'text-sky-400',
+        page: 1
+    } ,
+    js: {
+        name: 'Javascript',
+        icon: <SiJavascript/>,
+        color: 'text-yellow-400',
+        page: 1
+    } ,
+    bootstrap: {
+        name: 'Bootstrap',
+        icon: <SiBootstrap/>,
+        color: 'text-purple-400',
+        page: 1
+    } ,
+    c: {
+        name: 'C',
+        icon: <SiC/>,
+        color: 'text-purple-400',
+        page: 1
+    } ,
+    cpp: {
+        name: 'C++',
+        icon: <SiCplusplus/>,
+        color: 'text-blue-400',
+        page: 1
+    } ,
+    java: {
+        name: 'Java',
+        icon: <SiJava/>,
+        color: 'text-red-400',
+        page: 1
+    } ,
+    python: {
+        name: 'Python',
+        icon: <SiPython/>,
+        color: 'text-yellow-400',
+        page: 1
+    }, 
+    tailwind:{
+        name: 'Tailwind',
+        icon: <SiTailwindcss/>,
+        color: 'text-cyan-400',
+        page: 2
+    },
+    react: {
+        name: 'React',
+        icon: <SiReact/>,
+        color: 'text-sky-400',
+        page: 2
+    }, 
+    redux: {
+        name: 'Redux',
+        icon: <SiRedux/>,
+        color: 'text-purple-400',
+        page: 2
+    },
+    reactRouter: {
+        name: 'React Router',
+        icon: <SiReactrouter/>,
+        color: 'text-red-400',
+        page: 2
+    } ,
+    materialUi: {
+        name: 'Material UI',
+        icon: <SiMaterialui/>,
+        color: 'text-blue-400',
+        page: 2
+    }
+}
+
+const langicons = [
+    {
+        name: 'Html5',
+        icon: <SiHtml5/>,
+        color: 'text-orange-400',
+        page: 1
+    } ,
+    {
+        name: 'Css3',
+        icon: <SiCss3/>,
+        color: 'text-sky-400',
+        page: 1
+    } ,
+    {
+        name: 'Javascript',
+        icon: <SiJavascript/>,
+        color: 'text-yellow-400',
+        page: 1
+    } ,
+    {
+        name: 'Bootstrap',
+        icon: <SiBootstrap/>,
+        color: 'text-purple-400',
+        page: 1
+    } ,
+    {
+        name: 'C',
+        icon: <SiC/>,
+        color: 'text-purple-400',
+        page: 1
+    } ,
+    {
+        name: 'C++',
+        icon: <SiCplusplus/>,
+        color: 'text-blue-400',
+        page: 1
+    } ,
+    {
+        name: 'Java',
+        icon: <SiJava/>,
+        color: 'text-red-400',
+        page: 1
+    } ,
+    {
+        name: 'Python',
+        icon: <SiPython/>,
+        color: 'text-yellow-400',
+        page: 1
+    }, 
+    {
+        name: 'Tailwind',
+        icon: <SiTailwindcss/>,
+        color: 'text-cyan-400',
+        page: 2
+    },
+    {
+        name: 'React',
+        icon: <SiReact/>,
+        color: 'text-sky-400',
+        page: 2
+    }, 
+    {
+        name: 'Redux',
+        icon: <SiRedux/>,
+        color: 'text-purple-400',
+        page: 2
+    },
+    {
+        name: 'React Router',
+        icon: <SiReactrouter/>,
+        color: 'text-red-400',
+        page: 2
+    } ,
+    {
+        name: 'Material UI',
+        icon: <SiMaterialui/>,
+        color: 'text-blue-400',
+        page: 2
+    }
+]
+
+export default langicons;
