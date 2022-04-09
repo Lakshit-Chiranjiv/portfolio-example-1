bcc7c61a49ef1023c06c1cb14ce170cbdab4a953
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-09 20:16:04 +0530

    minor padding correction in about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div className='bg-bgclr w-11/12 m-10 mx-auto rounded-md p-8 bg-blue-400'>
+    <div className='bg-bgclr w-11/12 m-10 mx-auto rounded-md p-8 pt-4 bg-blue-400'>
         <h1 className="font-bold text-4xl my-6">About Me</h1>
         <p className='text-base'>Hey There!!👋 I am a Full Stack Web Developer. Like to learn new technologies and create something of my own using them. Also have some experience with Blockchain Development and Android Development. Currently quite immersed in creating new projects and trying out new frameworks and libraries. ReactJS is something that I have found most interesting and love to work with it. Still grasping over the ReactJS ecosystem and there is still a lot to learn and implement. Its a long journey and I am enjoying it developing and diving into something new always.✌💛💛!!!</p>
     </div>
