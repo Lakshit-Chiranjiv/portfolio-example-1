a87bfdb14dc6eb7d75cff7b3a25c9fa7c60c8e55
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-09 17:53:30 +0530

    minor correction - name alignment

--- src/components/EmoCard.jsx
+++ src/components/EmoCard.jsx
@@ -5,7 +5,7 @@ const EmoCard = () => {
   return (
     <div className='p-8 flex flex-col justify-center items-center bg-white rounded-md shadow-md w-1/3 min-w-[16rem]'>
         <img src={EmojiPic} alt="emojiPic" className='w-24'/>
-        <h1 className="text-2xl font-bold mt-2">Lakshit Chiranjiv Sagar</h1>
+        <h1 className="text-2xl font-bold mt-2 text-center">Lakshit Chiranjiv Sagar</h1>
         <div className="shadow-lg rounded p-4 text-center mt-4 bg-slate-100">
             <h4 className="font-medium">
                 Full Stack Developer | Designer
