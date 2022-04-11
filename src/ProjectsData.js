7674055001a05fce43d02d14c576a376f4f71626
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-12 00:08:08 +0530

    Demo Project Details added

---
+++ src/ProjectsData.js
@@ -0,0 +1,44 @@
+const projectData = [
+    {
+        name: 'Pokemons REST API(DB)',
+        type: 'b',
+        repo: '#',
+        deploy: '#',
+        techs: ['Node JS','Express JS','Mongo DB'],
+        page: 1
+    },
+    {
+        name: 'Amazon Prime UI clone',
+        type: 'f',
+        repo: '#',
+        deploy: '#',
+        techs: ['Tailwind CSS'],
+        page: 1
+    },
+    {
+        name: 'Pokemon MERN App',
+        type: 'fs',
+        repo: '#',
+        deploy: '#',
+        techs: ['Node JS','Express JS','Mongo DB','React JS','Material UI'],
+        page: 2
+    },
+    {
+        name: 'Crypto Price App',
+        type: 'f',
+        repo: '#',
+        deploy: '#',
+        techs: ['React JS'],
+        page: 2
+    },
+    {
+        name: 'Firebase Restaurent App',
+        type: 'fs',
+        repo: '#',
+        deploy: '#',
+        techs: ['React JS','Firebase','Bootstrap'],
+        page: 3
+    }
+]
+
+export default projectData;
