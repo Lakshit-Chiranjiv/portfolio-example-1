b501095ceca1bfed5883f9c72b3b7bdfd45e974e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-01 20:02:08 +0530

    phone flex-col

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = ({introRef,aboutRef,skillsRef,projectsRef,contactRef}) => {
   return (
     <div className='w-11/12 m-10 mx-auto text-white bg-green-400 rounded-md p-6'>
-        <div className="flex justify-center items-center gap-12">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-12">
             <div  className='hover:text-black hover:bg-white rounded p-4 py-6 cursor-pointer'>
                 <a className='text-xl font-bold' onClick={() => introRef.current.scrollIntoView({behavior:"smooth"})}>Home</a>
                 {/* <div className="h-2 w-2 bg-white m-3 rounded-full group-hover:animate-bounce"></div> */}
